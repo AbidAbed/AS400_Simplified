6fcfb4408926a6a6302570ee4de07351a4f0a9ef
ABID ABED <abidabed06@gmail.com>
2025-06-20 20:26:58 +0300

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,7 @@
+//------------------------ Dependencies ------------------------------//
+
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config({ path: "./.env" });
+const bodyparser = require("body-parser");
+
